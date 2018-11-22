1d35556e6a473ca13a1aa74c9b0a6caa56082d3c
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2018-11-22 13:16:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,13 +23,13 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content === '-مبلغ'){
-        message.channel.send('#credits @!Ṅę𝓕ớ ,Huler#2204 100000')
+        message.channel.send('#credits')
     }
 });
 
 client.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits @!Ṅę𝓕ớ ,Huler#2204 100000')
+        message.channel.send('#credits')
     }
 });
 
@@ -41,13 +41,13 @@ client2.on('message', message => {
 
 client2.on('message', message => {
     if(message.content === '-مبلغ'){
-        message.channel.send('#credits @!Ṅę𝓕ớ ,Huler#2204 100000')
+        message.channel.send('#credits')
     }
 });
 
 client2.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits @!Ṅę𝓕ớ ,Huler#2204 100000')
+        message.channel.send('#credits')
     }
 });
 
