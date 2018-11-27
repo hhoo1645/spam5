e413197cb9d2022a99754ee25b535eee24c8965f
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2018-11-27 14:38:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const jackeo = ['' , '434327219310034954' , '' , '407244267984125972'];
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -16,6 +17,83 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
+var prefix = "-";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!jackeo.includes(message.author.id)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command === "1say")  {
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **');
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+  
+ 
+
+if (command == "emb")    {
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **');
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor("RANDOM")
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
+
+
+
+
+client2.on('message', message => {
+var prefix = "-";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!jackeo.includes(message.author.id)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command === "3say")  {
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **');
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+  
+ 
+
+if (command == "emb")    {
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **');
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor("RANDOM")
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
+
+client.on('message', message => {
+    if(message.content === 'ريب'){
+        message.channel.send('#rep <@!434327219310034954>')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === 'ريب'){
+        message.channel.send('#rep <@!434327219310034954>')
+    }
+});
+
+client.on('message', message => {
     if(message.content === '-راتب'){
         message.channel.send('#daily')
     }
@@ -53,12 +131,12 @@ client2.on('message', message => {
 
 
 
-client.on('message', message => {
+client.on('message', message => { 
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**boom boom lol lol lol lol lol lol | يا عمي سباااااااااااااااااام = كريديييييييييييتتتتتتتتتتت لول لول لول انا بوت محشش صح **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -67,12 +145,12 @@ if (message.content === '!spam') {
       }
 });
 
-client2.on('message', message => {
+client2.on('message', message => { 
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**boom boom lol lol lol lol lol lol | يا عمي سباااااااااااااااااام = كريديييييييييييتتتتتتتتتتت لول لول لول انا بوت محشش صح **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -85,4 +163,4 @@ if (message.content === '!spam') {
 
 
 client.login(process.env.TOKEN);
-client2.login(process.env.TOKEN2);
+client2.login(process.env.TOKEN2)
