3bbb7e84c58924cfb29410d3ff689d693380ee10
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2018-12-18 14:35:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,8 +61,17 @@ var prefix = "-";
 
   let args = message.content.split(" ").slice(1);
 
-  if (command === "1say")  {
-  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **');
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+// !say
+  if (command === "say") {
           message.delete()
     message.channel.sendMessage(args.join(" ")).catch(console.error);
   }
